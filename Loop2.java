01e1a1251a367a0852a87352ddb58a09e0afae9b
md taha ahmed <md.taha.ahmed.projects@gmail.com>
2021-05-24 17:55:23 +0530

    loop II

--- Loop2.java
+++ Loop2.java
@@ -1,26 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Loop2 {
-    public static void main(String []argh){
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        ArrayList<Double[]> result = new ArrayList<Double[]>();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
-            for(int j=0;j<n;j++){
-                Double[] x = new Double[n];
-                // x[j-1]=(double) 0;
-                if(j==0){
-                    x[j]=a+(Math.pow(2, j)*b);
-                }else{
-                    x[j]=a+(Math.pow(2, j)*b)+x[j-1];
-                }
-                System.out.print(x[i]);
-
+            double sum = 0;
+            Double[] x = new Double[n];
+            for (int j = 0; j < n; j++) {
+                sum += (Math.pow(2, j) * b);
+                x[j] = a + sum;
             }
+            result.add(x);
         }
-        
+        for (Double[] xx : result) {
+            for (Double doubles : xx) {
+                System.out.print((int) Math.round(doubles) + " ");
+            }
+            System.out.println();
+        }
         in.close();
     }
 }
